9afffd8ce3667dfa1e2029f1115fa0bdd3ee09ba
99zhebin <zhebin.lim.2020@scis.smu.edu.sg>
2022-10-21 15:25:43 +0800

    Vue update

--- docs/adoption.js
+++ docs/adoption.js
@@ -1,6 +1,3 @@
-// Your web app's Firebase configuration
-// You can find yours at Firebase -> Project Overview -> Project Settings
-// -> SDK setup and configuration -> CDN
 const firebaseConfig = {
     apiKey: "AIzaSyAWvVP_h1HKDOSqjp9BFZ1ttifg_UhC0eQ",
     authDomain: "is216-webapp.firebaseapp.com",
@@ -10,13 +7,7 @@ const firebaseConfig = {
     messagingSenderId: "113592009297",
     appId: "1:113592009297:web:fd8286678c0c18abc3ee70"
     };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
 
-    // Load all Adoption Listings on first load
-
-
-    
 function loadDisplay() {
     console.log("--- loadDisplay() start ---")
     console.log("--- logInCheck() Start  ---")
@@ -41,88 +32,6 @@ function loadDisplay() {
           document.getElementById("login").setAttribute("href", url)
         }
       });
-    var petArray = firebase.database().ref('adoption').orderByChild("timestamp")
-    var pets = []
-    var content = ""
-    petArray.once('value').then((snapshot) => {
-    if(snapshot.exists()) {
-        console.log("Found")
-        console.log(snapshot.val())
-        pets = snapshot.val()
-        for (pet of pets){
-            let image = pet.image
-            let petName = pet.animalname
-            let species = pet.species
-            let age = pet.age
-            content += `
-            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 my-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="${image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h3 class="card-title">${petName}</h3>
-                        <h5 class="card-title">${species}</h5>
-                        <p class="card-text">Age: ${age}</a><br><br>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>
-            `   
-        }
-        if (content == ""){
-            content = "<h1 style='text-align:center;margin: 80px 0px'>There is currently nothing here</h1>"
-        }
-        document.getElementById("pets").innerHTML = content
-    }
-    else {
-        console.log("Not Found")
-    }
-    });
-}
-
-// Function to filter through adoption listings on button press
-
-function filter(){
-    var petArray = firebase.database().ref('adoption').orderByChild("timestamp")
-    var pets = []
-    var content = ""
-    var speciesFilter = document.getElementById("species").options[document.getElementById("species").selectedIndex].text
-    console.log(speciesFilter)
-    petArray.once('value').then((snapshot) => {
-    if(snapshot.exists()) {
-        console.log("Found")
-        console.log(snapshot.val())
-        pets = snapshot.val()
-        for (pet of pets){
-            let image = pet.image
-            let petName = pet.animalname
-            let species = pet.species
-            console.log(species)
-            let age = pet.age
-            if (species == speciesFilter){
-                content += `
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 my-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="${image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h3 class="card-title">${petName}</h3>
-                            <h5 class="card-title">${species}</h5>
-                            <p class="card-text">Age: ${age}</a><br><br>
-                            <a href="#" class="btn btn-primary">Details</a>
-                        </div>
-                    </div>
-                </div>
-                `   
-            }
-        }
-        if (content == ""){
-            content = "<h1 style='text-align:center;margin: 80px 0px'>No Matches Found</h1>"
-        }
-        document.getElementById("pets").innerHTML = content
-    }
-    else {
-        console.log("Not Found")
-    }
-    });
 }
 
 function logout() {
@@ -134,3 +43,69 @@ function logout() {
         // An error happened.
       });
 }
+
+const app = Vue.createApp({
+    data(){
+        return{
+            pets: [],
+
+            speciesFilter: "",
+
+            hdbFilter: "",
+
+            vaccinationFilter: "",
+
+            healthFilter: "",
+
+            filtered: [],
+        }
+    },
+
+    methods: {
+        filter(){
+            console.log("--- Start Filter ---")
+            this.filtered = []
+            for (pet of this.pets){
+                let species = pet.species
+                let vacStatus = pet.vaccination
+                let hdbStatus = pet.hdb
+                let healthStatus = pet.health
+                if (species == this.speciesFilter){
+                    if ((this.vaccinationFilter == true && vacStatus == "true") || (this.vaccinationFilter == false)){
+                        if ((this.hdbFilter == true && hdbStatus == "true") || (this.hdbFilter == false)){
+                            if ((this.healthFilter == true && healthStatus == "") || (this.healthFilter == false)){
+                                this.filtered.push(pet)
+                            }
+                        }
+                    }
+                }
+            }
+            if (this.filtered.length == 0){
+                this.filtered = "not found"
+            }
+        },
+
+        url(email,petName){
+            let url = "Adopt_page.html?name=" + email + "-" + petName
+            return url
+        },
+    },
+
+    mounted() {
+        console.log("--- Initialise Firebase ---")
+        firebase.initializeApp(firebaseConfig);
+        let petArray = firebase.database().ref('adoption').orderByChild("timestamp")
+        petArray.once('value').then((snapshot) => {
+        if(snapshot.exists()) {
+            console.log("Found")
+            console.log(snapshot.val())
+            this.pets = snapshot.val()
+        }
+        else {
+            console.log("Not Found")
+        }
+        })
+    }
+})
+
+const vm2 = app.mount("#content")
