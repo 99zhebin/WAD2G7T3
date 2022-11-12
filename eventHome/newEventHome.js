7d5f1305c9f09dd4133925faa610f64d828ae590
WAN_LIN TIAN <WANLIN.TIAN.2021@scis.smu.edu.sg>
2022-11-12 16:18:26 +0800

    pagination

--- eventHome/newEventHome.js
+++ eventHome/newEventHome.js
@@ -224,10 +224,34 @@ const app = Vue.createApp({
         }
         });
 
-    },
-})
+        // pagination
+        var pageSize = 1;
+        var pageCount = this.filtered.length / pageSize;
+        console.log(this.filtered.length)
+
+        for(var i = 0 ; i<pageCount;i++){
+            $("#pagin").append('<li><a href="#">'+(i+1)+'</a></li> ');
+        }
+
+        $("#pagin li").first().find("a").addClass("current")
+        showPage = function(page) {
+            this.filtered.hide();
+            this.filtered.each(function(n) {
+            if (n >= pageSize * (page - 1) && n < pageSize * page)
+                $(this).show();
+            });        
+        }
+
+        showPage(1);
+
+        $("#pagin li a").click(function() {
+            $("#pagin li a").removeClass("current");
+            $(this).addClass("current");
+            showPage(parseInt($(this).text())) 
+        });
+
+            },
+        })
 
 const vm2 = app.mount("#container")
-
-
 
